07679e19ca4490ac6dacf22bc239003e73d0c20b
Skills Academy <student03@MckenzieCamacho.fios-router.home>
2022-05-07 14:21:27 -0400

    header & searchbox created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import { Route, Routes } from 'react-router-dom';
+// import Homepage from './components/Homepage'
+import SearchBox from './components/SearchBox';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
+        <SearchBox/>
+
+
+      {/* <Routes>
+        <Route path="/" element={<Homepage/>}/>
+      </Routes> */}
       </header>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-dom'
+
+const Header = () =>{
+    return(
+        <div>
+            <h1>
+                Take a Hike Connecticut
+            </h1>
+        </div>
+    )
+}
+export default Header;
