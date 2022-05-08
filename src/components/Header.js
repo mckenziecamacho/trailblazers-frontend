ce78a909f6a5ed07df1c57964ef5dd98caad63e4
Boscobrand <Tony.Bosco@boscobrand.com>
2022-05-08 17:36:32 -0400

    1) router revision to index 2) Revision work to app and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,27 @@ import { Link } from 'react-router-dom';
 const Header = () =>{
     return(
         <div className='header'>
+            
             <header>
-            <h1>
-                Take a Hike Connecticut
-            </h1>
+                <div>
+                    <h1>Take a Hike Connecticut</h1>
+                </div>
+
             <nav className='navbar'>
-                <Link to="/home">
-                <h1>Home</h1>
-                </Link>
+
+                <div className='home'>
+                    <Link to="/"><h4>Home</h4></Link>
+                </div>
+
                 <div className='search-box'>
-                    <Link to='/search'>
-                        <h1>Search</h1>
-                    </Link>
-                 </div>
-                 <div className='about'> 
-                    <Link to='/about'>
-                     <h1>About</h1>
-                 </Link>
-                 </div>
-             </nav>
+                    <Link to='/search'><h1>Search</h1></Link>
+                </div>
+                
+                <div className='about'> 
+                    <Link to='/About'><h1>About</h1></Link>
+                </div>
+            </nav>
+            
             </header>
         </div>
 
