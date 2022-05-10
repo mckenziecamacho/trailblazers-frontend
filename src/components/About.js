30ad4d986032956cbdd036c069fcea3c79518f21
Skills Academy <student03@MckenzieCamacho.fios-router.home>
2022-05-09 20:22:26 -0400

    about

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,6 @@ const About = () => {
             Take a Hike Connecticut is a website dedicated to individuals who enjoy hiking and want to get more information about trails in the Connecticut Area or individuals who want to get into hiking and exploring the outdoors.
             The goal of our website is to make hiking fun and accessible to any one who is open to the adventure. We want to encourage people to be active, enjoy nature and go outside and fresh air and sunlight. Connecticut is filled with such wonderful and beautiful places to hike.
             </p>
-            <p>adding another p tag to commit changes</p>
         </div>
     )
 }
