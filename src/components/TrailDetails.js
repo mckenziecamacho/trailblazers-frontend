1d505f8768bd2a5749d6fd1964dccb72090d62ca
Skills Academy <student03@MckenzieCamacho.fios-router.home>
2022-05-11 10:26:57 -0400

    updated traildetails

--- src/components/TrailDetails.js
+++ src/components/TrailDetails.js
@@ -2,41 +2,57 @@ import React, { useEffect } from 'react';
 import {useParams} from "react-router-dom";
 import { useState } from 'react';
 
-const url = 'https://take-a-hike-ct.herokuapp.com/api/trail/';
+const TrailDetails=(props) => {
+      const[trail,setTrails] =useState("");
+      const trailId = useParams()._id
+      const trails = props.data.filter(trails => trails._id === trailId)[0]
+      console.log(trails)
+      useEffect(() => {
+        const url = 'https://take-a-hike-ct.herokuapp.com/api/trail';
 
-function TrailDetails() {
-    const [trail, setTrail] = useState();
-    //useState allows you to have state variables in functional components
-    const trailId = useParams()._id
-    //returns on object of key/value pairs of url parameters
-    useEffect(() => {
-    //tells react that your component needs to do something after render
-        fetch(url + trailId)
-            .then((res) => res.json())
-            .then((res) => {
-                setTrail(res)
-                console.log(res)
-            })
-            .catch((error => {
-            // console.log(error)
-            })
-        )
-    },[])
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const result = await response.json();
+                setTrails(result.data)
+                console.log(result);
+                } catch (error) {
+                console.log("error", error);
+                }
+        };
+
+        fetchData();
+      },[]);
+
+
 
     
     return(
         <div className='trail-details'>
-            <div className='name'>  <b>Name: </b>{trail? trail.name : ""}</div>
-            <div className='location'> <b>Location: </b>{trail? trail.location : ""} </div>
-            <div className='image'> <b>Image: </b>{trail? trail.image : ""} </div>
-            <div className='difficulty'> <b>Difficulty: </b>{trail? trail.difficulty : ""} </div>
-            <div className='miles'> <b>Miles: </b> {trail? trail.miles : ""}</div>
-            <div className='terrain'> <b>Terrain: </b> {trail? trail.terrain : ""}</div>
-            <div className='duration'> <b>Duration: </b> {trail? trail.duration : ""}</div>
-            <div className='overview'> <b>Overview:</b> {trail? trail.overview : ""}</div>
-            <div className='website'> <b>Website: </b> {trail? trail.website : ""}</div>
-            <div className='review'> <b>Review: </b> {trail? trail.review : ""}</div>
+            <div className='name'>  <b>Name: </b>{trails? trails.name : ""}</div>
+            <div className='location'> <b>Location: </b>{trails? trails.location : ""} </div>
+            <div className='image'> <b>Image: </b>{trails? trails.image : ""} </div>
+            <div className='difficulty'> <b>Difficulty: </b>{trails? trails.difficulty : ""} </div>
+            <div className='miles'> <b>Miles: </b> {trails? trails.miles : ""}</div>
+            <div className='terrain'> <b>Terrain: </b> {trails? trails.terrain : ""}</div>
+            <div className='duration'> <b>Duration: </b> {trails? trails.duration : ""}</div>
+            <div className='overview'> <b>Overview:</b> {trails? trails.overview : ""}</div>
+            <div className='website'> <b>Website: </b> {trails? trails.website : ""}</div>
+            <div className='review'> <b>Review: </b> {trails? trails.review : ""}</div>
         </div>
-    )
-}
+    )}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default TrailDetails;
